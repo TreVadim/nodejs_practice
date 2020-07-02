8f4af40151444d2906454f92d8043fd8330abf3f
Goloviychuk Vadim <vadim.goloviychuk@gmail.com>
2020-07-02 23:40:49 +0300

    101 lesson is done

--- app.js
+++ app.js
@@ -1,37 +1,24 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-// const expressHbs = require("express-handlebars");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
 
 const app = express();
-
-// Pug
-// app.set("view engine", "pug");
-
-// HandleBars
-// app.engine("hbs", expressHbs({
-//     extname: '.hbs',
-//     defaultLayout: "main-layout",
-//     layoutsDir: "views/layouts/"
-// }));
-// app.set("view engine", "hbs");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "public"))); //for static path
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render("404", {pageTitle: "Page Not Found!"});
-});
+app.use(errorController.get404);
 
 app.listen(4545);
 
-//88 lesson
+//101 lesson
